8e4ef34e0614b2d604c2185315583435e7204508
znibb <znibb@zkylark.se>
2023-07-31 00:05:15 +0200

    Cosmetic path fixes

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ if [ ! -f .env ]; then
   cp files/.env.example .env
 fi
 if [ ! -f files/mosquitto.log ]; then
-  touch files/mosquitto.log
+  touch ./files/mosquitto.log
 fi
 if [ ! -f files/mosquitto.passwd ]; then
-  touch files/mosquitto.passwd
+  touch ./files/mosquitto.passwd
 fi
