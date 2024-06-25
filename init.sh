5daaaaa1795c403bbc4afd53ff851dca381eccbc
znibb <znibb@zkylark.se>
2024-06-25 13:14:42 +0200

    Changed logging to output to stdout instead of log file

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@
 if [ ! -f .env ]; then
   cp -v ./files/.env.example .env
 fi
-if [ ! -f files/mosquitto.log ]; then
-  touch ./files/mosquitto.log
-fi
 if [ ! -f files/mosquitto.passwd ]; then
   touch ./files/mosquitto.passwd
 fi
